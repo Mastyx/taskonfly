3c50af685eac0db37365d4f24cfad95770a561fb
Mastyx <mastyx.house@gmail.com>
2025-04-13 19:25:54 +0200

    depl

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,29 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+//
+///** @type {import('@sveltejs/kit').Config} */
+//const config = {
+//	kit: {
+//		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+//		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+//		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+//		adapter: adapter()
+//	}
+//};
+//
+//export default config;
+//
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from 'sveltekit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+const dev = process.env.NODE_ENV === 'development';
+
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/taskonfly',
+    },
+  },
 };
 
-export default config;
